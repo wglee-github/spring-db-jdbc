81dc429a21297110bdf60fc1c9308e1a64e9a11b
wglee <amd0223@gmail.com>
2023-07-13 23:54:46 +0900

    섹션 3. 트랜잭션 이해
    1.트랜잭션 - 개념 이해
    2.데이터베이스 연결 구조와 DB 세션
    3.트랜잭션 - DB 예제1 - 개념 이해
    4.트랜잭션 - DB 예제2 - 자동 커밋, 수동 커밋
    5.트랜잭션 - DB 예제3 - 트랜잭션 실습
    6.트랜잭션 - DB 예제4 - 계좌이체
    7.DB 락 - 개념 이해
    8.DB 락 - 변경
    9.DB 락 - 조회

---
+++ src/test/java/hello/jdbc/service/MemberServiceV1Test.java
@@ -0,0 +1,77 @@
+package hello.jdbc.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import hello.jdbc.connection.ConnectionConst;
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV1;
+
+/**
+ * 
+ * 기본동작, 트랜잭션이 없어서 문제 발생
+ */
+class MemberServiceV1Test {
+
+	private static final String MEMBER_A = "memberA";
+	private static final String MEMBER_B = "memberB";
+	private static final String MEMBER_EX = "ex";
+	
+	private MemberRepositoryV1 repository;
+	private MemberServiceV1 service;
+	
+	@BeforeEach
+	void before() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource(ConnectionConst.URL, ConnectionConst.USERNAME, ConnectionConst.PASSWORD);
+		repository = new MemberRepositoryV1(dataSource);
+		service = new MemberServiceV1(repository);
+	}
+	
+//	@Test
+	@DisplayName("정상이체")
+	void accountTransfer() throws SQLException {
+		
+		Member memberA = new Member(MEMBER_A, 10000);
+		Member memberB = new Member(MEMBER_B, 10000);
+		
+		repository.save(memberA);
+		repository.save(memberB);
+		
+		service.accountTransfer(memberA.getMemberId(), memberB.getMemberId(), 2000);
+		
+		Member findMemberA = repository.findById(memberA.getMemberId());
+		Member findMemberB = repository.findById(memberB.getMemberId());
+		
+		assertThat(findMemberA.getMoney()).isEqualTo(8000);
+		assertThat(findMemberB.getMoney()).isEqualTo(12000);
+	}
+	
+	@Test
+	@DisplayName("이체 중 예외 발생")
+	void accountTransferEx() throws SQLException {
+		
+		Member memberA = new Member(MEMBER_A, 10000);
+		Member memberEX = new Member(MEMBER_EX, 10000);
+		
+		repository.save(memberA);
+		repository.save(memberEX);
+		
+//		service.accountTransfer(memberA.getMemberId(), memberEX.getMemberId(), 2000);
+		
+		Assertions.assertThrows(IllegalStateException.class, () -> service.accountTransfer(memberA.getMemberId(), memberEX.getMemberId(), 2000));
+		
+		Member findMemberA = repository.findById(memberA.getMemberId());
+		Member findMemberB = repository.findById(memberEX.getMemberId());
+		
+		assertThat(findMemberA.getMoney()).isEqualTo(8000);
+		assertThat(findMemberB.getMoney()).isEqualTo(10000);
+	}
+
+}
